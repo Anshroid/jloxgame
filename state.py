59d0727822961eb642b2792b1cbf7d7b03a88dfd
anshroid <anshroid07@gmail.com>
2026-09-14 15:52:43 +0100

    Event API + pausing / game time emulation

--- state.py
+++ state.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 from types import GenericAlias
 from discord import ApplicationContext, Member, Role, Thread
 from enum import Enum
-from typing import Any, Callable, Concatenate, Coroutine, Self, cast, get_args, get_origin, get_type_hints
+from typing import Any, Callable, Concatenate, Coroutine, Self, cast, get_args, get_origin, overload
+from inspect import Parameter, Signature, signature
 import pathlib, json, time, asyncio, random
 
 Status = Enum("Status", "INIT SETUP RUNNING PAUSED END")
@@ -12,6 +13,9 @@ Status = Enum("Status", "INIT SETUP RUNNING PAUSED END")
 ######################################## EVENTS ########################################
 
 type Serializable = str | int | float | bool | list[Serializable] | tuple[Serializable] | dict[str, Serializable]
+type EventFunc[ContextType, **Params, ReturnType] = Callable[Concatenate[ContextType, Params], ReturnType] 
+type CallbackWithReturnedVal[ContextType, **Params, ReturnType] = Callable[Concatenate[ContextType, ReturnType, Params], Coroutine[Any, Any, None]] 
+type CallbackWithoutReturnedVal[ContextType, **Params] = Callable[Concatenate[ContextType, Params], Coroutine[Any, Any, None]]
 
 def _serializable(_type: type) -> bool:
     if _type in [str, int, float, bool]:
@@ -36,9 +40,14 @@ class EventInstance:
     def to_dict(self): return {"__type__": self.__type__, "__time__": self.__time__, "args": self.args, "kwargs": self.kwargs}
 
 class Event[ContextType: GameContext, **Params, ReturnType]:
-    def __init__(self, func: Callable[Concatenate[ContextType, Params], ReturnType], callback: Callable[[ContextType, ReturnType], Coroutine[None, None, None]] | None = None) -> None:
+    def __init__(
+        self, 
+        func: EventFunc[ContextType, Params, ReturnType], 
+        callback: CallbackWithReturnedVal[ContextType, Params, ReturnType] | CallbackWithoutReturnedVal[ContextType, Params] | None = None
+    ) -> None:
         self.func = func
         self.callback = callback
+        self.has_return_type = signature(func, eval_str=True).return_annotation != None
     
     def __get__(self, gctx: ContextType, _=None) -> BoundEvent[ContextType, Params, ReturnType]:
         return BoundEvent(self, gctx)
@@ -64,39 +73,57 @@ class BoundEvent[ContextType: GameContext, **Params, ReturnType]:
         
         print(f"[{gctx.thread_id} | info] adding event {self.event_type()}")
 
-        inst = self.get_instance(gctx.game_time_now(), *args, **kwargs)
-        
         try:
             ret = self.event.func(gctx, *args, **kwargs)
         except Exception as e:
-            print(f"[{gctx.thread_id} | info] exception raised in event {inst.to_dict()}, reloading recommended")
             asyncio.create_task(gctx.save(pathlib.Path() / "save")) # TODO: hardcoded
             raise e
 
+        inst = self.get_instance(gctx.game_time_now(), *args, **kwargs)
         gctx.event_log.append(inst)
 
-        if self.event.callback: asyncio.create_task(self.event.callback(gctx, ret))
+        if self.event.callback:
+            if self.event.has_return_type:
+                callback = cast(CallbackWithReturnedVal[ContextType, Params, ReturnType], self.event.callback)
+                asyncio.create_task(callback(gctx, ret, *args, **kwargs))
+            else:
+                callback = cast(CallbackWithoutReturnedVal[ContextType, Params], self.event.callback)
+                asyncio.create_task(callback(gctx, *args, **kwargs))
         return ret
     
     def __repr__(self) -> str: return f"<jloxgame.state.BoundEvent object on context {self.gctx} of type {self.event_type()}>"
 
-# TODO: callback
-# def event[ContextType: GameContext, **Params, ReturnType](callback: Callable[[ContextType, ReturnType], Coroutine[None, None, None]] | None = None):
-def event[ContextType: GameContext, **Params, ReturnType](func: Callable[Concatenate[ContextType, Params], ReturnType]) -> Event[ContextType, Params, ReturnType]:
-    if asyncio.iscoroutinefunction(func):
-        raise ValueError(f"Invalid function {func} marked as event: function may not be async!")
+@overload
+def event[ContextType: GameContext, **Params, ReturnType](*, callback: None = None) -> Callable[[EventFunc[ContextType, Params, ReturnType]], Event[ContextType, Params, ReturnType]]: ...
+@overload
+def event[ContextType: GameContext, **Params, ReturnType](*, callback: CallbackWithoutReturnedVal[ContextType, Params]) -> Callable[[EventFunc[ContextType, Params, ReturnType]], Event[ContextType, Params, ReturnType]]: ...
+@overload
+def event[ContextType: GameContext, **Params, ReturnType](*, callback: CallbackWithReturnedVal[ContextType, Params, ReturnType]) -> Callable[[EventFunc[ContextType, Params, ReturnType]], Event[ContextType, Params, ReturnType]]: ...
 
-    types = get_type_hints(func)
-    for label, _type in types.items():
-        if label in ["self", "return"]: continue
+def event[ContextType: GameContext, **Params, ReturnType](*, callback: CallbackWithReturnedVal[ContextType, Params, ReturnType] | CallbackWithoutReturnedVal[ContextType, Params] | None = None):
+    def inner(func: Callable[Concatenate[ContextType, Params], ReturnType], /) -> Event[ContextType, Params, ReturnType]:
+        if asyncio.iscoroutinefunction(func):
+            raise ValueError(f"Invalid function {func} marked as event: function may not be async!")
 
-        if not _serializable(_type):
-            raise ValueError(f"Invalid function {func} marked as event: parameter {label} ({_type}) is not serializable!")
+        sig = signature(func, eval_str=True)
 
-    print(f"[jloxgame | info] registered event {func.__name__}")
+        if sig.return_annotation == Signature.empty:
+            raise ValueError(f"Invalid function {func} marked as event: return type must be annotated!")
 
-    return Event(func)
-    # return inner
+        for name, param in sig.parameters.items():
+            if name == "self": continue
+
+            if param.annotation == Parameter.empty:
+                raise ValueError(f"Invalid function {func} marked as event: parameter {name} type must be annotated!")
+
+            if not _serializable(param.annotation):
+                raise ValueError(f"Invalid function {func} marked as event: parameter {name} of type {param.annotation} is not serializable!")
+
+        print(f"[jloxgame | info] registered event {func.__name__}")
+
+        return Event(func, callback)
+    
+    return inner
 
 
 ######################################## MODELS ########################################
@@ -151,15 +178,16 @@ class GameContext(ABC):
         
         self.thread_id: int = -1
         self.thread: Thread | None = None
+
+        self.paused = True
+        self.loading = False
         
-        self.init_time = time.time_ns() // 1000
-        self.last_update = self.init_time
+        self.init_time = time.time_ns() // 1000000
+        self.last_update = 0
         self.pause_duration = 0
 
         self.scheduler_task: asyncio.Task[None] | None = None
 
-        self.errored = False
-        
         self.rng = random.Random(self.init_time)
         
         self.event_log: list[EventInstance] = []
@@ -181,16 +209,17 @@ class GameContext(ABC):
             raise ValueError(f"Invalid team id provided: {id}")
         
     def game_time_now(self) -> int: # TODO: during loading, spoof this
-        return (time.time_ns() // 1000) - self.init_time - self.pause_duration
+        if self.paused or self.loading: return self.last_update
+        return (time.time_ns() // 1000000) - self.init_time - self.pause_duration
     
     def unix_ms_to_game_time(self, unix_ms: int) -> int: # TODO: Maybe remove these?
-        return (unix_ms * 1000) - self.init_time - self.pause_duration
+        return unix_ms - self.init_time - self.pause_duration
     
     def message_id_to_game_time(self, message_id: int) -> int:
         return self.unix_ms_to_game_time((message_id >> 22) + 1420070400000)
     
     def game_time_to_unix_ms(self, game_time: int) -> int:
-        return (game_time // 1000) + self.init_time + self.pause_duration
+        return (game_time // 1000000) + self.init_time + self.pause_duration
 
     def actualise_instance(self, inst: EventInstance):
         bound_event: BoundEvent[Self, Any, Any] = getattr(self, inst.__type__)
@@ -200,8 +229,6 @@ class GameContext(ABC):
         except Exception as e:
             asyncio.create_task(self.save(pathlib.Path() / "save"))
             raise ValueError(f"[{self.thread_id} | info] exception raised in event {inst.to_dict()}, reloading recommended") from e
-
-        self.event_log.append(inst)
 
     def schedule_event[**Params, ReturnType](self, h: int, m: int, s: int, event: BoundEvent[Self, Params, ReturnType], *args: Params.args, **kwargs: Params.kwargs) -> None:
         """Schedule an event to be added to the event log.
@@ -220,6 +247,7 @@ class GameContext(ABC):
         # technically a min-heap would be optimal, but who cares, right?
     
     async def schedule_tick(self) -> None:
+        if self.paused: return
         if len(self.scheduled_events) > 0:
             # lock not needed here since there is no async during the state modification (this coro cannot be interrupted)
             while len(self.scheduled_events) > 0 and self.game_time_now() > self.scheduled_events[0].__time__:             
@@ -244,7 +272,6 @@ class GameContext(ABC):
                 raise TypeError(f"Could not encode {obj} into JSON!")
 
     async def save(self, dir: pathlib.Path) -> None:
-        if self.errored: return
         print(f"[{self.thread_id} | info] saving game")
         with open(dir / f"{self.thread_id}.json", "w") as f:
             data: dict[str, Any] = {
@@ -264,21 +291,34 @@ class GameContext(ABC):
             gctx.teams = [Team.from_dict(team_dict) for team_dict in data["teams"]]
             
             gctx.init_time = data["init_time"]
-            gctx.last_update = data["last_update"]
             
             gctx.thread_id = thread_id
+
+            gctx.loading = True
 
             gctx.rng = random.Random(gctx.init_time)
             
             gctx.event_log = []
             for event_dict in data["event_log"]:
                 inst = EventInstance(event_dict["__type__"], event_dict["__time__"], event_dict["args"], event_dict["kwargs"])
-                gctx.actualise_instance(inst)
+                if inst.__time__ > data["last_update"]:
+                    print(f"[{gctx.thread_id} | info] scheduling event {inst.__type__}")
+                    gctx.scheduled_events.append(inst)
+                    gctx.scheduled_events.sort(key=lambda e: e.__time__)
+                else:
+                    gctx.last_update = inst.__time__
+                    gctx.actualise_instance(inst)
+            
+            gctx.loading = False
 
-            gctx.__reload__(gctx.game_time_now() - gctx.last_update)
+            gctx.__reload__(gctx.game_time_now() - data["last_update"])
             
             return gctx
-    
-    @event
+
+    @event()
     def __reload__(self, pause_duration: int) -> None:
         self.pause_duration += pause_duration
+
+    def unpause(self) -> None:
+        self.paused = False
+        self.pause_duration += self.game_time_now() - self.last_update
